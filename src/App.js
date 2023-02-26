8349abf392894d8c2d7baaa55f15ba4afeb3baf2
Marcio Caraballo <marcio.caraballososa@gmail.com>
2023-02-26 15:57:46 -0300

    Project bootstrap

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.scss';
+import Species from './Species';
+
+const API_URL = 'https://swapi.dev/api/films/2/';
+const SPECIES_IMAGES = {
+  droid:
+    'https://static.wikia.nocookie.net/starwars/images/f/fb/Droid_Trio_TLJ_alt.png',
+  human:
+    'https://static.wikia.nocookie.net/starwars/images/3/3f/HumansInTheResistance-TROS.jpg',
+  trandoshan:
+    'https://static.wikia.nocookie.net/starwars/images/7/72/Bossk_full_body.png',
+  wookie:
+    'https://static.wikia.nocookie.net/starwars/images/1/1e/Chewbacca-Fathead.png',
+  yoda: 'https://static.wikia.nocookie.net/starwars/images/d/d6/Yoda_SWSB.png',
+};
+const CM_TO_IN_CONVERSION_RATIO = 2.54;
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Empire Strikes Back - Species Listing</h1>
+      <div className="App-species" />
+    </div>
+  );
+}
+
+export default App;
