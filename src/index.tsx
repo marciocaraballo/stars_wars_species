588c1583b317a3aae2a042d005b8f035a610d9d8
Marcio Caraballo <marcio.caraballososa@gmail.com>
2023-02-27 00:51:08 -0300

    Adds back strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,8 @@ import App from './App';
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
-
 root.render(
-    <App />
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
